3443102be6eeb73175f742226b55db46ff594e5e
delafildzania <42663150+delafildzania@users.noreply.github.com>
2018-10-12 10:11:15 +0700

    Add files via upload

---
+++ output.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+$konek = mysqli_connect('localhost','root','','d_jurnal6');
+
+$sql="SELECT * FROM t_jurnal6";
+$query=mysqli_query($konek, $sql);
+$halo= mysqli_fetch_array($query);
+
+echo "NIM : " .$halo['nim']."<br>";
+echo "Nama : " .$halo['nama']."<br>";
+echo "kelas : " .$halo['kelas']."<br>";
+echo "Jenis Kelamin : " .$halo['jk']."<br>";
+echo "Hobi : " .$halo['hobi']."<br>";
+echo "Fakultas : " .$halo['fakultas']."<br>";
+echo "Alamat : " .$halo['alamat']."<br>";
+
+ 
+?>
+
+<a href ="edit.php"> Edit.</a>
